2ea674512c57b8fae00567f1c9bf6e5d7e8db9a8
Iago Almeida <iagoti03@gmail.com>
2022-02-05 17:56:13 -0300

    novos arquivos

---
+++ src/modules/users/controllers/ForgotPasswordController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import SendForgotPasswordEmailService from '../services/SendForgotPasswordEmailService';
+
+export default class ForgotPasswordController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { email } = request.body;
+
+    const ForgotPasswordEmail = new SendForgotPasswordEmailService();
+
+    await ForgotPasswordEmail.execute({
+      email,
+    });
+
+    return response.status(204).json();
+  }
+}
